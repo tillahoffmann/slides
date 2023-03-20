c6dbc725803ce5af4626d455c066c7f6c63ed017
Till Hoffmann <tillahoffmann@gmail.com>
2023-03-20 15:35:15 -0400

    Allow additional frontmatter properties.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md") as fp:
 
 setup(
     name="markdown-slides",
-    version="0.1.7",
+    version="0.1.8",
     packages=find_packages(),
     install_requires=[
         "flask",
